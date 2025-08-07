30c1bcd4b99849c60bc106f97d46e00afbfc7d68
unknown <tauhidsaifi26748@gmail.com>
2025-08-07 13:09:22 +0530

    Added Call Now Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,10 +30,10 @@ function Hero() {
 
 
             <a
-              href="#"
+              href="tel:+919876543210" onClick={() => setIsOpen(false)}
               className="bg-white dark:bg-gray-800 border border-green-600 text-green-700 dark:text-green-300 hover:bg-green-50 dark:hover:bg-gray-700 px-6 py-3 rounded-md text-lg shadow transition"
             >
-              Learn More
+              Call Now
             </a>
           </div>
         </div>
